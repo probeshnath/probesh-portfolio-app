05db47ced09c1892e1f7c843eedf2f57cab3f25f
probeshnath <probeshn@gmail.com>
2023-12-10 23:19:34 +0600

    tailwind css and react-router-dom added

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider} from "react-router-dom";
+import MainLayout from './pages/MainLayout';
+import ErrorPage from './pages/ErrorPage';
+import Home from './pages/Home';
+import About from './pages/About';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout></MainLayout> ,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>
+      },
+      {
+        path: "about",
+        element: <About></About>
+      },
+    ]
+
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div>
+            <h1 className="text-3xl text-red-200 font-bold underline">
+                Hello world!
+            </h1>
+        </div>
+    )
+}
+
+export default Home
